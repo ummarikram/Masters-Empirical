028a33a7f92f0690090f5426c39a9c70129aba1a
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2024-02-05 21:32:56 +0500

    Ethereum Setup

---
+++ monitoring/falcon.ts
@@ -0,0 +1,48 @@
+import { falcon } from 'falcon-crypto';
+import { PostQuantumAlgorithm } from './base';
+
+export class FalconAlgorithm implements PostQuantumAlgorithm {
+
+    async generateKeys(): Promise<{ privateKey: Uint8Array; publicKey: Uint8Array; }> {
+        return await falcon.keyPair()
+    }
+
+    async sign(message: Uint8Array, privateKey: Uint8Array): Promise<Uint8Array> {
+        return await falcon.signDetached(message, privateKey)
+    }
+
+    async verify(signature: Uint8Array, message: Uint8Array, publicKey: Uint8Array): Promise<Boolean> {
+        return await falcon.verifyDetached(signature, message, publicKey)
+    }
+
+    async performSigning(noOfMessages: number, messageLengthBytes: number) {
+        
+        const keyPair = await this.generateKeys();
+        const message = new Uint8Array(messageLengthBytes);
+
+        // Array to store promises for signing and verifying
+        const signingPromises = [];
+
+        // Start Clock
+        const signStart = performance.now();
+
+        for (let i = 0; i < noOfMessages; i++) {
+            // Push promises for signing
+            signingPromises.push(this.sign(message, keyPair.privateKey));
+        }
+
+        // Wait for all messages to be signed
+        await Promise.all(signingPromises);
+
+        // End Clock
+        const signEnd = performance.now();
+
+        // Measure the total time for signing
+        const totalSignTime = (signEnd - signStart) / 1000;
+
+        // Calculate and log the timings
+        console.log(`Falcon Total time for ${noOfMessages} messages: ${totalSignTime} seconds`);
+      
+        console.log(`Falcon Average time per message: ${totalSignTime/noOfMessages} seconds`);
+    }
+}
