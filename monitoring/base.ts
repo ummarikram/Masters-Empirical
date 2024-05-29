c9e7705082ba02a05db502f20c8bce7c75c40063
Ummar Ikram <ummarikram@gmail.com>
2024-05-29 16:09:27 +0500

    Uploaded Dataset

--- monitoring/base.ts
+++
@@ -1,6 +0,0 @@
-export interface PostQuantumAlgorithm {
-    generateKeys(): Promise<{ privateKey: Uint8Array; publicKey: Uint8Array; }>
-    sign(message: Uint8Array, privateKey: Uint8Array): Promise<Uint8Array>
-    verify(signature: Uint8Array, message: Uint8Array, publicKey: Uint8Array): Promise<Boolean>;
-    performSigning(noOfMessages: number, messageLengthBytes: number): Promise<void>;
-}
--- readme.md
+++ readme.md
@@ -5,10 +5,4 @@
 pip install -r requirements.txt --user
 ```
 
-## POWER CONSUMPTION APPROACH
-
-The test consists of signing *x* messages each of *y* bytes for *z* seconds. The power spike is calculated using [OpenHardwareMonitor](https://openhardwaremonitor.org) for the following specifications:
-
-1. **Processor**: Intel i5-1135G7
-2. **Ram**: 12 GB DDR4 3200 MHZ
 
